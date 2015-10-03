f116b94ab0cc661916b8c1a97fd6f8aaaba818de
Cid <cdubs8600@gmail.com>
2015-10-03 15:16:29 -0700

    flushing out some geo functions

--- vendor/geo.js
+++ vendor/geo.js
@@ -1,38 +1,60 @@
 
 $(function() {
 
-    function putLatLng(geo) {
+    $('body').on('click', '.getlocationbtn', function(e) {
+        var $btnEl = $(e.currentTarget);
+        if ($btnEl.hasClass('disabled')) {
+            return;
+        } else {
+            $btnEl.addClass('disabled');
+            showLoader();
+            getLatLng($btnEl);
+        }
+        getLatLng()
+    });
+
+    function showLoader() {
+        $('#loader').show();
+    }
+    function hideLoader() {
+        $('#loader').hide();
+    }
+
+    function putLatLng($btnEl, geo) {
         var lat = geo.lat;
         var lng = geo.lng;
         // put lat lng in local storage
+        hideLoader();
+        $btnEl.removeClass('disabled');
     }
 
-    function getLatLng() {
+    function getLatLng($btnEl) {
         // get lat lng from local storage
+        // check local before req to speed up after first check
+
 
         // if (!latLng) {
-            //reqLatLng(cb);
+            //reqLatLng($btnEl, cb);
         //}
 
     }
 
-    function reqLatLng(cb) {
+    function reqLatLng($btnEl, cb) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var geo = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
             console.dir(location);
-            cb(geo)
-            return location;
+            cb(geo);
         });
     }
-
-getLatLng();
 
     if (!($(html).hasClass('gelocation'))) {
         // goelocation not supported
         // offer some other way to tap the API
+    } else {
+        getLatLng();
     }
 
 });
