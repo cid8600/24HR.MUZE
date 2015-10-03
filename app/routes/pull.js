12221f6ba7ddd3506604540f0c7e0f5a2740685c
Cid <cdubs8600@gmail.com>
2015-10-03 13:01:31 -0700

    initial comit with bourbon env

---
+++ app/routes/pull.js
@@ -0,0 +1,7 @@
+export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON('https://api.github.com/repos/emberjs/ember.js/pulls').then(function(data) {
+      return data.splice(0, 3);
+    });
+  }
+});
